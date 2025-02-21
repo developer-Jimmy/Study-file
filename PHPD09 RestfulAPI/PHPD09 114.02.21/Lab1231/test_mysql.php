939178b09d106a33a99ef4fa287467fe31512580
developer-Jimmy <g12347825@gmail.com>
2025-02-21 16:46:25 +0800

    RestfulAPI第四版更新

---
+++ PHPD09 RestfulAPI/PHPD09 114.02.21/Lab1231/test_mysql.php
@@ -0,0 +1,38 @@
+<?php
+
+
+    // 底下範例程式碼針對上課使用，請在實際開發時進行調整
+    header("Content-type: application/json; charset=UTF-8");
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+ 
+    $mydb = new mysqli('localhost','root','', 'school');
+    $mydb->set_charset('utf8');
+    $method = $_SERVER["REQUEST_METHOD"];
+
+
+    // 獲得資料庫的資料有很多種方式；這裡是範例，請比照先前上課方式改寫
+    switch($method){
+        case "GET":
+            $sql = 'SELECT * FROM student';
+            $result = $mydb->query($sql);
+            $data = [];
+            while($row = $result->fetch_object()){
+                $data[] = $row;
+            }
+            echo json_encode($data);
+            break;
+
+
+        case "POST":
+            $student_name =  $_POST['student_name'];
+            $sql = 'INSERT INTO student (student_name) VALUES (?)';            
+            $stmt = $mydb->prepare($sql);
+            $stmt->bind_param('s', $student_name);
+            $stmt->execute();
+            echo json_encode(["message"=>"insert: " .$mydb -> insert_id . "---" .$student_name]);
+            break;
+    }
+
+
+?>
