ec290eb4ceabb0d6ab8c0f0fa560afe3ae1d8c8c
developer-Jimmy <g12347825@gmail.com>
2025-02-27 16:22:33 +0800

    React第三版

---
+++ PHPD09 React/PHPD09 114.02.27/React/react_part_1/react_part_1/More_JS_for_React/demo_module/myMain.js
@@ -0,0 +1,11 @@
+// import {x,y as yy} from "./myToolBox.js"
+// alert("x = "+ x);
+// alert("y = "+ yy);
+
+// import{other, other2} from "./myToolBox.js";
+// import defaultItem from "./myToolBox.js";
+// import hello, {x, y} from "./myToolBox.js";
+import {hello, x, y} from "./myToolBox.js";
+
+hello();
+alert(x);
